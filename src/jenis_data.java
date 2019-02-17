f2c7f51795f99b34cd7948999de7451d277f3cb4
pc18 <pc18@pc18-PC>
2019-02-17 12:25:30 +0800

    third commit

---
+++ src/jenis_data.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author pc 19
+ */
+public class jenis_data {
+    public static void main(String[] args){
+        int bilangan=139;
+        String barangan="Biskut";
+        double harga=2.55;
+        boolean status=true;
+        float suhu=4.23468923f;
+        char pilihan='Y';
+        System.out.println("Stok semasa :"+bilangan +"unit");
+        System.out.println("Nama Barangan :"+barangan")
+        System.out.println("Harga seunit:RM"+harga")
+        System.out.println("Status barangan:"+status")
+        System.out.println("Suhu Bilik :"+suhu")
+        System.out.println("Semua maklumat adalah benar:"+pilihan")   
+     
+    }
+}
